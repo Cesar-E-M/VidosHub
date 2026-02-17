c4cab0b74228f0ae77c4c10eacd6f584d8bd9063
Cesar-E-M <cmunozmagdaleno@gmail.com>
2026-02-17 18:07:26 -0500

    mass

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -199,7 +199,7 @@ export const Header = () => {
                       <hr className="my-1 border-gray-100" />
                       <button
                         onClick={onOpen}
-                        className="sm:hidden block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                        className="sm:hidden block px-4 py-2 text-sm text-black hover:bg-gray-100"
                       >
                         <Upload className="h-5 w-5" />
                         Subir Video
